bd4d56a5aa1f8ed38002beda3c5f877e275fc7bf
OmerrKose <omer.kose@ibtech.com.tr>
2021-09-04 16:45:40 +0300

    Added JSON object parsing and accessing

--- app/src/main/java/com/example/simpleapicalling/MainActivity.kt
+++ app/src/main/java/com/example/simpleapicalling/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -35,7 +36,8 @@ class MainActivity : AppCompatActivity() {
             var connection: HttpURLConnection? = null
 
             try {
-                val url = URL("https://run.mocky.io/v3/9f69b0cd-f367-479d-a219-a1a7f36b948c")
+                /** This URL is to be replaced if needed t access a different JSON object */
+                val url = URL("https://run.mocky.io/v2/5e3939193200006700ddf815")
 
                 connection = url.openConnection() as HttpURLConnection
                 connection.doInput = true // do we get data
@@ -43,9 +45,7 @@ class MainActivity : AppCompatActivity() {
 
                 val httpResult: Int = connection.responseCode
 
-                /**
-                 * check if the returned result from the server is OK (200)
-                 */
+                /** check if the returned result from the server is OK (200) */
                 if (httpResult == HttpURLConnection.HTTP_OK) {
                     val inputStream = connection.inputStream
 
@@ -94,12 +94,50 @@ class MainActivity : AppCompatActivity() {
             if (result != null) {
                 Log.i("JSON RESPONSE RESULT", result)
             }
+
+            /**
+             * This piece of code is to parse the received JSON object and display it on the Log screen
+             * created jsonObject is to access to the JSON that was returned from the URL (result)
+             */
+            val jsonObject = JSONObject(result)
+
+            val userId = jsonObject.optInt("user_id")
+            val name = jsonObject.optString("name")
+            val message = jsonObject.optString("message")
+
+            Log.i("User id ", userId.toString())
+            Log.i("Name ", name)
+            Log.i("Message", message)
+
+            /**
+             * To access the inner level of the JSON object that was returned apply the same process to the profileDetailsObject
+             * Access to the data list that is embedded in the profile details
+             * print all the related information via a for loop
+             */
+            val profileDetailsObject = jsonObject.optJSONObject("profile_details")
+            val profileIsCompleted = profileDetailsObject?.optBoolean("is_profile_completed")
+
+            Log.i("Is Profile Completed", profileIsCompleted.toString())
+
+            val dataListArray = jsonObject.optJSONArray("data_list")
+            Log.i("Data List Size", dataListArray?.length().toString())
+
+            for (item in 0 until dataListArray!!.length()) {
+                Log.i("Value $item", dataListArray[item].toString())
+
+                val dataItemObject: JSONObject = dataListArray[item] as JSONObject // Store each read data from the JSON object
+                val itemId = dataItemObject.optInt("id")
+                val value = dataItemObject.optString("value")
+                Log.i("Item ID", itemId.toString())
+                Log.i("Item Value", value)
+
+
+            }
+
             cancelProgressDialog()
         }
 
-        /**
-         * This function is to set the dialog for dialog box
-         */
+        /** This function is to set the dialog for dialog box */
         private fun showProgressDialog() {
             customProgressDialog = Dialog(this@MainActivity)
             customProgressDialog.setContentView(R.layout.dialog_custom_progress)
@@ -107,9 +145,7 @@ class MainActivity : AppCompatActivity() {
             customProgressDialog.show()
         }
 
-        /**
-         * This function is to dismiss the dialog box
-         */
+        /** This function is to dismiss the dialog box */
         private fun cancelProgressDialog() {
             customProgressDialog.dismiss()
         }
